7dda4f6ce857fb6ac8372643b8bc5cc69ac6c186
saurabh-kd-singh <sksingh5087@gmail.com>
2023-05-29 12:03:49 +0530

    User state created in Register and Login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import './Login.css'
 
 function Login() {
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+
+    setUser({
+      ...user,
+      [name]: value
+    });
+  }
+
+
   return (
     <div className="login">
       <h1>Login</h1>
-      <input type="text" placeholder='Enter your Email'></input>
-      <input type="password" placeholder='Enter your Password'></input>
+      <input type="text" name='email' value={user.email} placeholder='Enter your Email' onChange={ handleChange }></input>
+      <input type="password" name='password' value={user.password} placeholder='Enter your Password' onChange={ handleChange }></input>
       <div className="button">Login</div>
       <div>or</div>
       <div className="button">Register</div>
